3ced74940567f9ca03b66c165ee5caabbee6a3d9
darkProgramador001dat <squareroom01000010@gmail.com>
2025-11-24 02:36:32 -0300

    Update conditionalServer.js

--- conditionalServer.js
+++ conditionalServer.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require('express');           // Biblioteca Framework Express
 const bodyParser = require('body-parser');    // Biblioteca para realizar parser de formularios.
 
-const app = express();
+const app = express();  // Instancia servidor
 
 // Utilizando o metodo, para receber o corpo da requisição, Sim.
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json());  // Converter os dados em JSON() da requisição para servidor
 
-const users = [{ username: 'darkProgramador', password: '02040801' }];    // Criando um objeto com as credenciais
+const users = [{ username: 'darkProgramador', password: '02040801' }];    // Criando um objeto com as credenciais em formato legivel para humanos
 
+// Criando uma ROTA raiz
 app.get('/', (request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type','text/html');
@@ -39,13 +40,14 @@ app.post('/login', (request, response) => {
 
   if(username === users[0].username && password === users[0].password)  // Realizando uma igualdade entre a requisição e o indice do objeto
   {
-    response.status(200).send(`<h2>Login authorized 200!</h2>`);
+    response.status(200).send(`<h2>Login authorized 200!</h2>`);  
   }else
   {
     response.status(401).send(`<h2>Login incorrect username or password</h2>`);
   }
 });
 
+// Vinculando PORTA e iniciando o servidor
 const PORT = 5500;
 app.listen(PORT, () => {
   console.clear();
